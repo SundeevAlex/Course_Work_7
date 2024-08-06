eeffd8466e554e2058b36dfaec9c22b1c163a016
Aleksandr Sundeev <navigator97@mail.ru>
2024-08-06 14:13:44 +0700

    Add update & delete

--- habits/urls.py
+++ habits/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from habits.apps import HabitsConfig
-from habits.views import HabitsCreateApiView
+from habits.views import HabitsCreateApiView, HabitsUpdateApiView, HabitsDestroyApiView
 
 
 app_name = HabitsConfig.name
 
 urlpatterns = [
-    path("habit/create/", HabitsCreateApiView.as_view(), name="habits_create"),
+    path("habits/create/", HabitsCreateApiView.as_view(), name="habits_create"),
+    path("habits/update/<int:pk>/", HabitsUpdateApiView.as_view(), name="habits_update"),
+    path("habits/delete/<int:pk>/", HabitsDestroyApiView.as_view(), name="habits_delete"),
 
 ]
--- habits/views.py
+++ habits/views.py
@@ -1,6 +1,18 @@
 from rest_framework import generics
+
+from habits.models import Habits
 from habits.serializers import HabitsSerializer
 
 
 class HabitsCreateApiView(generics.CreateAPIView):
+    serializer_class = HabitsSerializer
+
+
+class HabitsUpdateApiView(generics.UpdateAPIView):
+    queryset = Habits.objects.all()
+    serializer_class = HabitsSerializer
+
+
+class HabitsDestroyApiView(generics.DestroyAPIView):
+    queryset = Habits.objects.all()
     serializer_class = HabitsSerializer
