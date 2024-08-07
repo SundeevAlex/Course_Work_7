3f73741150d57e0703255f992054cbc978579722
Aleksandr Sundeev <navigator97@mail.ru>
2024-08-07 12:54:29 +0700

    Add permissions

--- habits/views.py
+++ habits/views.py
@@ -2,21 +2,41 @@ from rest_framework import generics
 
 from habits.models import Habits
 from habits.paginations import HabitsListPagination
+from habits.permissions import IsCreator
 from habits.serializers import HabitsSerializer
 
 
 class HabitsCreateApiView(generics.CreateAPIView):
     serializer_class = HabitsSerializer
 
+    def perform_create(self, serializer):
+        """Делаем пользователя автором привычки."""
+        new_habit = serializer.save()
+        new_habit.owner = self.request.user
+        new_habit.save()
+
 
 class HabitsUpdateApiView(generics.UpdateAPIView):
     queryset = Habits.objects.all()
     serializer_class = HabitsSerializer
 
+    permission_classes = (IsCreator,)
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_authenticated:
+            return Habits.objects.filter(owner=user)
+
 
 class HabitsDestroyApiView(generics.DestroyAPIView):
     queryset = Habits.objects.all()
     serializer_class = HabitsSerializer
+    permission_classes = (IsCreator,)
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_authenticated:
+            return Habits.objects.filter(owner=user)
 
 
 class HabitsPublicListApiView(generics.ListAPIView):
@@ -35,3 +55,10 @@ class HabitsListApiView(generics.ListAPIView):
     queryset = Habits.objects.all()
     serializer_class = HabitsSerializer
     pagination_class = HabitsListPagination
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser:
+            return Habits.objects.all()
+        elif user.is_authenticated:
+            return Habits.objects.filter(owner=user)
