792d29c41252d9bf73b2c91219508461f088f14e
Aleksandr Sundeev <navigator97@mail.ru>
2024-08-07 11:19:32 +0700

    Add validators

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from habits.models import Habits
+from habits.validators import NotConnectionHabitAndRewardValidator, DurationValidator, CombinationValidator, \
+    NotRewardOrConnectionHabitValidator, FrequencyValidator
 
 
 class HabitsSerializer(serializers.ModelSerializer):
@@ -8,3 +10,11 @@ class HabitsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habits
         fields = "__all__"
+
+        validators = [
+            NotConnectionHabitAndRewardValidator('connection_habit', 'reward'),
+            DurationValidator('duration'),
+            CombinationValidator('connection_habit', 'habit_is_pleasant'),
+            NotRewardOrConnectionHabitValidator('habit_is_pleasant', 'connection_habit', 'reward'),
+            FrequencyValidator('number_of_executions'),
+        ]
